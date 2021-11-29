153592aaf3eccb00a4c2aa63d2c0e9933e99bc60
yunchong <19226491@qq.com>
2021-11-29 22:55:08 +0800

    添加测试代码

---
+++ examples.js/calc/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"path/filepath"
+
+	"github.com/breakcount/go-sciter"
+	"github.com/breakcount/go-sciter/window"
+)
+
+func main() {
+	w, err := window.New(sciter.SW_TITLEBAR|sciter.SW_RESIZEABLE|sciter.SW_CONTROLS|sciter.SW_MAIN|sciter.SW_ENABLE_DEBUG, nil)
+	if err != nil {
+		log.Fatal("Create Window Error: ", err)
+	}
+	fullpath, err := filepath.Abs("./examples.js/calc/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.LoadFile(fullpath)
+	w.Show()
+	w.Run()
+}
+
+
