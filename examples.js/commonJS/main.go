247e5cd6f717512d3a7cbef305c630460f340042
yunchong <19226491@qq.com>
2021-11-29 23:31:04 +0800

    添加测试

---
+++ examples.js/commonJS/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"path/filepath"
+
+	"github.com/breakcount/go-sciter"
+	"github.com/breakcount/go-sciter/window"
+)
+
+func main() {
+	DefaultRect := &sciter.Rect{0, 0, 1280, 800}
+	w, err := window.New(sciter.SW_TITLEBAR|sciter.SW_RESIZEABLE|sciter.SW_CONTROLS|sciter.SW_MAIN|sciter.SW_ENABLE_DEBUG, DefaultRect)
+	if err != nil {
+		log.Fatal("Create Window Error: ", err)
+	}
+	fullpath, err := filepath.Abs("./examples.js/commonJS/example/test.htm")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.LoadFile(fullpath)
+	w.Show()
+	w.Run()
+}
+
+
---
+++ examples.js/fetch(AJAX)/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"path/filepath"
+
+	"github.com/breakcount/go-sciter"
+	"github.com/breakcount/go-sciter/window"
+)
+
+func main() {
+	DefaultRect := &sciter.Rect{0, 0, 1280, 800}
+	w, err := window.New(sciter.SW_TITLEBAR|sciter.SW_RESIZEABLE|sciter.SW_CONTROLS|sciter.SW_MAIN|sciter.SW_ENABLE_DEBUG, DefaultRect)
+	if err != nil {
+		log.Fatal("Create Window Error: ", err)
+	}
+	testHtml := "test-fetch-sciter"
+	fullpath:= ""
+	switch testHtml {
+	case "test-fetch":
+		fullpath, err = filepath.Abs("./examples.js/fetch(AJAX)/test-fetch.htm")
+	case "test-fetch-sciter":
+		fullpath, err = filepath.Abs("./examples.js/fetch(AJAX)/test-fetch-sciter.htm")
+	case "test-json":
+		fullpath, err = filepath.Abs("./examples.js/fetch(AJAX)/test-json.htm")
+	case "test-post-json":
+		fullpath, err = filepath.Abs("./examples.js/fetch(AJAX)/test-post-json.htm")
+	case "test-upload":
+		fullpath, err = filepath.Abs("./examples.js/fetch(AJAX)/test-upload.htm")
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.LoadFile(fullpath)
+	w.Show()
+	w.Run()
+}
+
+
