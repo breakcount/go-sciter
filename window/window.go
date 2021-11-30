00ad1a9c5585ea6adbe7b2218f2c4f9983126c75
yunchong <19226491@qq.com>
2021-11-30 19:47:12 +0800

    修正配置文件

--- window/window.go
+++ window/window.go
@@ -15,7 +15,7 @@ func (w *Window) run() {
 }
 
 // https://github.com/golang/go/wiki/LockOSThread
-// https://github.com/sciter-sdk/go-sciter/issues/201
+// https://github.com/breakcount/go-sciter/issues/201
 func init() {
 	runtime.LockOSThread()
 }
